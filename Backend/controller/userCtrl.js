1a3bf8282c7df87c8318bffd04558f532f09debe
Mikaelao <kolajidedamilare@gmail.com>
2024-09-07 13:41:14 +0100

    subvilla_backend

---
+++ Backend/controller/userCtrl.js
@@ -0,0 +1,20 @@
+const User = require("../models/userModel");
+
+const asyncHandler = require("express-async-handler");
+
+// create users
+const createUser = asyncHandler(async (req, res) => {
+    const email = req.body.email;
+  const findUser = await User.findOne({ email: email });
+
+  if (!findUser) {
+    const newUser = await User.create(req.body);
+    res.json(newUser);
+  } else {
+    throw new Error("User Already Exists");
+  }
+});
+
+module.exports = {
+  createUser,
+};
---
+++ Backend/models/userModel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const crypto = require("crypto");
+// Schema of the Mongo model
+var userSchema = new mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+}
+);
+
+//Export the model
+module.exports = mongoose.model("User", userSchema);

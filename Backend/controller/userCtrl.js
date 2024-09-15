028cec0b45112692ed59db2c8f4f927e4e2813f1
Mikaelao <kolajidedamilare@gmail.com>
2024-09-15 17:07:24 +0100

    backend

--- Backend/controller/userCtrl.js
+++ Backend/controller/userCtrl.js
@@ -1,6 +1,8 @@
 const User = require("../models/userModel");
 
 const asyncHandler = require("express-async-handler");
+const { generateToken } = require("../config/jwtToken");
+const jwt = require("jsonwebtoken");
 
 // create users
 const createUser = asyncHandler(async (req, res) => {
@@ -15,6 +17,69 @@ const createUser = asyncHandler(async (req, res) => {
   }
 });
 
+
+//login a new user
+const loginUserCtrl = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  // check if user already exists or not
+  const findUser = await User.findOne({ email });
+  if (findUser && (await findUser.isPasswordMatched(password))) {
+    res.json({
+      _id: findUser?._id,
+      firstname: findUser?.firstname,
+      lastname: findUser?.lastname,
+      email: findUser?.email,
+      mobile: findUser?.mobile,
+      token: generateToken(findUser?._id),
+    });
+  } else {
+    throw new Error("invalid password");
+  }
+});
+
+// Get details of all users
+const getallUser = asyncHandler(async (req, res) => {
+  try {
+    const getUsers = await User.find().populate();
+    res.json(getUsers);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+// Get a user
+const getaUser = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const getaUser = await User.findById(id);
+    res.json({
+      getaUser,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+// Delete a user
+const deleteaUser = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deleteaUser = await User.findByIdAndDelete(id);
+    res.json({
+      deleteaUser,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+
 module.exports = {
   createUser,
+  loginUserCtrl,
+  getallUser,
+  getaUser,
+  deleteaUser,
 };
--- Backend/routes/authRoute.js
+++ Backend/routes/authRoute.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const {createUser} = require("../controller/userCtrl");
+const {
+    createUser,
+    loginUserCtrl,
+    getallUser,
+    getaUser,
+    deleteaUser,
+
+} = require("../controller/userCtrl");
 router.post("/register", createUser);
+router.post("/login", loginUserCtrl);
+router.get("/all-users", getallUser);
+router.get("/:id", getaUser);
+router.delete("/:id", deleteaUser);
 
 module.exports = router;
